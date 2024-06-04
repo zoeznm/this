cc23e42f3513e6e7dd33a1f040b61e8a621bf569
zoeznm <bomikim046@gmail.com>
2024-06-04 10:52:29 +0900

    work-2

--- work-2.js
+++ work-2.js
@@ -3,7 +3,22 @@ class WorkTwo {
     this.a=a;
     this.b=b;
   }
+  set a(value) {
+    if (typeof(value)==="number") {
+      return this._a = value;
+    } else {
+      throw new Error('a는 숫자만 입력 가능합니다.');
+    }
+  }
+
+  set b(value) {
+    if (typeof(value)==="number") {
+      return this._b = value;
+    } else {
+      throw new Error('b는 숫자만 입력 가능합니다.');
+    }
+  }
 }
 
-let test = new WorkTwo (1,2);
+let test = new WorkTwo ('ㅇㅇ',2);
 console.log(test);
