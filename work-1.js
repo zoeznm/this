ce7b977d68ba645f59d628bad5cb61dec460be0d
zoeznm <bomikim046@gmail.com>
2024-06-04 10:35:50 +0900

    &&

--- work-1.js
+++ work-1.js
@@ -1,8 +1,8 @@
 function worOne (a,b) {
-  if(typeof(a)==="number") {
-    if(typeof(b)==="number") {
+  if(typeof(a)==="number" && typeof(b)==="number" ) {
       let local = a+b;
       return local;
-    }
+  } else {
+    console.log ('매개변수 잘못작성됨');
   }
 };
